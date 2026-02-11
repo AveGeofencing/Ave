5b3853f94f65132792f35a091c7ad452b0727cdf
Adedara Adeloro <courageadedara@gmail.com>
2026-02-11 21:16:01 +0100

    refactor: - rename repo - allow all database functions to use conn object passed during call, instead of declaring its own during module instantiation. Helps to handle connection sessions and atomicity of transactions

--- app/repositories/geofence_repository.py
+++ app/repositories/geofence_repository.py
@@ -7,27 +7,33 @@ from ..models import Geofence, AttendanceRecord
 from ..schemas import AttendanceRecordModel, GeofenceCreateModel
 
 from fastapi import Depends
-from sqlalchemy import and_, func
+from sqlalchemy import and_, func, select, update
 from sqlalchemy.orm import selectinload
-from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-DatabaseDependency = Annotated[AsyncSession, Depends(get_db_session)]
+DATABASE_DEPENDENCY = Annotated[AsyncSession, Depends(get_db_session)]
 
 
 class GeofenceRepository:
-    def __init__(self, session: AsyncSession):
-        self.session = session
 
+    @classmethod
     async def create_geofence(
-        self,
+        cls,
+        conn: AsyncSession,
         geofence: GeofenceCreateModel,
         fence_code: str,
         creator_matric: str,
         start_time_utc,
         end_time_utc,
-        NOW,
+        now,
     ):
+        if now < start_time_utc:
+            status = "scheduled"
+        elif start_time_utc <= now < end_time_utc:
+            status = "active"
+        else:
+            status = "inactive"
+
         new_geofence = Geofence(
             fence_code=fence_code,
             name=geofence.name,
@@ -38,39 +44,38 @@ class GeofenceRepository:
             fence_type=geofence.fence_type,
             start_time=start_time_utc,
             end_time=end_time_utc,
-            status=(
-                "scheduled"
-                if NOW < start_time_utc
-                else "active" if start_time_utc <= NOW < end_time_utc else "inactive"
-            ),
-            time_created=NOW,
+            status=status,
+            time_created=now,
         )
 
-        self.session.add(new_geofence)
-        await self.session.commit()
-        await self.session.refresh(new_geofence)
+        conn.add(new_geofence)
+        await conn.flush()
+        await conn.refresh(new_geofence)
 
         return new_geofence
 
-    async def get_all_geofences(self):
+    @classmethod
+    async def get_all_geofences(cls, conn: AsyncSession):
         stmt = select(Geofence)
-        result = await self.session.execute(stmt)
+        result = await conn.execute(stmt)
 
         geofences = result.scalars().all()
         return geofences
 
-    async def get_all_geofences_by_user(self, user_id: str):
+    @classmethod
+    async def get_all_geofences_by_user(cls, user_id: str, conn: AsyncSession):
         stmt = select(Geofence).filter(Geofence.creator_matric == user_id)
-        result = await self.session.execute(stmt)
+        result = await conn.execute(stmt)
         geofence_by_user = result.scalars().all()
 
         return geofence_by_user
 
-    async def get_geofence(self, course_title: str, date: datetime) -> Geofence:
+    @classmethod
+    async def get_geofence(cls, course_title: str, date: datetime, conn: AsyncSession) -> Geofence:
         stmt = (
             select(Geofence)
             .options(selectinload(Geofence.student_attendances))
-            .filter(
+            .where(
                 and_(
                     Geofence.name == course_title,
                     func.date(Geofence.start_time) == date.date(),
@@ -78,27 +83,30 @@ class GeofenceRepository:
             )
         )
 
-        result = await self.session.execute(stmt)
+        result = await conn.execute(stmt)
         geofence = result.scalars().one_or_none()
 
         return geofence
 
-    async def get_geofence_by_fence_code(self, fence_code: str):
+    @classmethod
+    async def get_geofence_by_fence_code(cls, fence_code: str, conn: AsyncSession):
         stmt = (
             select(Geofence)
             .options(selectinload(Geofence.student_attendances))
-            .filter(Geofence.fence_code == fence_code)
+            .where(Geofence.fence_code == fence_code)
         )
-        result = await self.session.execute(stmt)
+        result = await conn.execute(stmt)
         geofence = result.scalars().one_or_none()
         return geofence
 
+    @classmethod
     async def record_geofence_attendance(
-        self,
+        cls,
         attendance: AttendanceRecordModel,
         user_matric: str,
         geofence_name: str,
         matric_fence_code: str,
+        conn: AsyncSession
     ):
         attendance = AttendanceRecord(
             user_matric=user_matric,
@@ -107,43 +115,39 @@ class GeofenceRepository:
             timestamp=datetime.now(ZoneInfo("UTC")),
             matric_fence_code=matric_fence_code,
         )
-        self.session.add(attendance)
-        await self.session.commit()
-        await self.session.refresh(attendance)
+        conn.add(attendance)
+        await conn.flush()
+        await conn.refresh(attendance)
 
         return attendance
 
+    @classmethod
     async def get_attendance_record_for_student_for_geofence(
-        self, matric_fence_code: str
+        cls, matric_fence_code: str, conn: AsyncSession
     ):
         stmt = select(AttendanceRecord).filter(
             AttendanceRecord.matric_fence_code == matric_fence_code
         )
-        result = await self.session.execute(stmt)
+        result = await conn.execute(stmt)
         attendance_record = result.scalars().one_or_none()
 
         return attendance_record
 
-    async def deactivate_geofence(self, geofence_name: str, date: datetime):
-        geofence = await self.get_geofence(course_title=geofence_name, date=date)
+    @classmethod
+    async def deactivate_geofence(cls, geofence_code: str, conn: AsyncSession):
+        stmt = update(Geofence).where(Geofence.fence_code == geofence_code).values(status="inactive")
+        await conn.execute(stmt)
 
-        geofence.status = "inactive"
 
-        self.session.add(geofence)
-        await self.session.commit()
-        await self.session.refresh(geofence)
-
-    async def get_geofence_attendances(self, fence_code: str):
+    @classmethod
+    async def get_geofence_attendances(cls, fence_code: str, conn: AsyncSession):
         stmt = (
             select(AttendanceRecord)
             .options(selectinload(AttendanceRecord.user))
-            .filter(AttendanceRecord.fence_code == fence_code)
+            .where(AttendanceRecord.fence_code == fence_code)
         )
-        result = await self.session.execute(stmt)
+        result = await conn.execute(stmt)
 
         attendances = result.scalars().all()
         return attendances
 
-
-def get_geofence_repository(db_session: DatabaseDependency) -> GeofenceRepository:
-    return GeofenceRepository(session=db_session)
