572c96e3a389161642c4bedf687c74d6b6c1a5f1
Adedara Adeloro <courageadedara@gmail.com>
2026-02-11 21:13:35 +0100

    refactor: - rename module

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,75 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy.future import select
+from sqlalchemy.orm import selectinload
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import or_, update
+from pydantic import EmailStr
+
+from ..schemas.user_schema import UserCreateModel
+from ..models import User
+from ..utils import logger
+
+
+class UserRepository:
+    @classmethod
+    async def get_user_by_id(cls, user_id: str, conn: AsyncSession):
+        stmt = select(User).where(User.id == user_id)
+        result = await conn.execute(stmt)
+        return result.scalar_one_or_none()
+
+    @classmethod
+    async def get_user_by_email_or_matric(
+        cls, conn: AsyncSession, email: str | EmailStr = None, matric: str = None,
+    ) -> User:
+        stmt = (
+            select(User)
+            .options(selectinload(User.attendances))
+            .where(or_(User.email == email, User.user_matric == matric))
+        )
+        result = await conn.execute(stmt)
+
+        user: User = result.scalars().first()
+
+        return user
+
+    @classmethod
+    async def create_new_user(cls, user: UserCreateModel, password_hash: str, conn: AsyncSession):
+        new_user: User = User(
+            email=user.email,
+            user_matric=user.user_matric,
+            role=user.role,
+            username=user.username,
+            hashed_password=password_hash,
+            is_email_verified=False
+        )
+
+        conn.add(new_user)
+        await conn.flush()
+        await conn.refresh(new_user)
+
+        return new_user
+
+    @classmethod
+    async def change_user_password(cls, user_id: str, new_hashed_password: str, conn: AsyncSession):
+        stmt = update(User).where(User.id == user_id).values(hashed_password=new_hashed_password).returning(User.email)
+        result = await conn.execute(stmt)
+        row = result.scalar()
+        logger.debug(f"Password changed for user {row}")
+        return row
+
+    @classmethod
+    async def verify_user(cls, conn: AsyncSession, user_id: str):
+        stmt = (
+            update(User)
+            .where(User.id == user_id)
+            .where(User.is_email_verified == False)
+            .values(is_email_verified=True).returning(User.id)
+        )
+        result = await conn.execute(stmt)
+        row = result.scalar()  # Returns the first affected row or None
+
+        if row:
+            return True  # user was verified just now
+        else:
+            return False  # user was already verified (no row changed)
