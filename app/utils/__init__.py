326cee5967049ec2e7ab5ee9bd6d588c055662e4
Adedara Adeloro <courageadedara@gmail.com>
2026-02-11 21:09:47 +0100

    feat: - add custom logger

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,8 +1,9 @@
-from .config import get_app_settings, get_email_settings, Settings, EmailSettings
-from .GeofenceUtils import check_user_in_circular_geofence
+from .geofence_utils import check_user_in_circular_geofence
 from .constants import (
     PASSWORD_MIN_LENGTH,
     PASSWORD_RESET_TOKEN_EXPIRY_MINUTES,
     EMAIL_SUBJECTS,
     ERROR_MESSAGES,
 )
+
+from .logger import logger
