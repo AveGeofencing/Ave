55471b02585fc0330df12f278dc03f9c93eb4fe8
Adedara Adeloro <courageadedara@gmail.com>
2026-04-23 11:47:17 +0100

    feat: link college and department models using relationships

--- app/models/college.py
+++ app/models/college.py
@@ -1,10 +1,16 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING, List
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..database import Base
 
+if TYPE_CHECKING:
+    from .department import Department
 
 class College(Base):
     __tablename__ =  "college"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
+
+    departments: Mapped[List["Department"]] = relationship("Department", back_populates="college")
--- app/models/department.py
+++ app/models/department.py
@@ -1,8 +1,12 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..database import Base
 
+if TYPE_CHECKING:
+    from .college import College
 
 class Department(Base):
     __tablename__ =  "department"
@@ -10,3 +14,5 @@ class Department(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     college_id: Mapped[int] = mapped_column(ForeignKey("college.id"))
+
+    college: Mapped["College"] = relationship("College", back_populates="departments")
